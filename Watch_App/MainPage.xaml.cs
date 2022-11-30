ec4ab31423a44faa10c093aacb0b22820fbe6dce
Svetallana <cherniisvetapl@gmail.com>
2022-11-30 22:15:56 +0200

    initial commit

---
+++ Watch_App/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Watch_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuTapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OverlayTapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
